e44341dddf4201abdb0a85d21263abef5cf63e93
James Rishe <JLRishe@users.noreply.github.com>
2022-01-28 19:37:51 +0900

    Touchups

--- src/extensions/minimalFieldCustomizer/MinimalFieldCustomizerFieldCustomizer.ts
+++ src/extensions/minimalFieldCustomizer/MinimalFieldCustomizerFieldCustomizer.ts
@@ -28,6 +28,8 @@ export default class MinimalFieldCustomizerFieldCustomizer
 
   @override
   public onInit(): Promise<void> {
+    console.log('onInit');
+
     // Add your custom initialization to this method.  The framework will wait
     // for the returned promise to resolve before firing any BaseFieldCustomizer events.
     Log.info(LOG_SOURCE, 'Activated MinimalFieldCustomizerFieldCustomizer with properties:');
@@ -38,8 +40,10 @@ export default class MinimalFieldCustomizerFieldCustomizer
 
   @override
   public onRenderCell(event: IFieldCustomizerCellEventParameters): void {
+    console.log('onRenderCell');
+
     // Use this method to perform your custom cell rendering.
-    const text: string = `${this.properties.sampleText}: ${event.fieldValue}`;
+    const text: string = `! - ${event.fieldValue}`;
 
     const minimalFieldCustomizer: React.ReactElement<{}> =
       React.createElement(MinimalFieldCustomizer, { text } as IMinimalFieldCustomizerProps);
@@ -49,6 +53,8 @@ export default class MinimalFieldCustomizerFieldCustomizer
 
   @override
   public onDisposeCell(event: IFieldCustomizerCellEventParameters): void {
+    console.log('onDisposeCell');
+
     // This method should be used to free any resources that were allocated during rendering.
     // For example, if your onRenderCell() called ReactDOM.render(), then you should
     // call ReactDOM.unmountComponentAtNode() here.
